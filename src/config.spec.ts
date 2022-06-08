b45a6b222702c8a328684fa40d824c030e5a77ea
Juan Cazala <juancazala@gmail.com>
2022-06-08 17:12:45 -0300

    feat: added config.is(env) and config.getEnv() helpers

--- src/config.spec.ts
+++ src/config.spec.ts
@@ -15,69 +15,91 @@ const configByEnv: ConfigByEnv = {
   },
 }
 
-describe('when using a config', () => {
+describe('when geting using a config', () => {
   const { env } = process
 
   afterEach(() => {
     process.env = env
   })
 
-  describe('and all the envs are defined', () => {
-    describe('and the env is Env.DEVELOPMENT', () => {
-      describe('and getting value of a variable that exists', () => {
-        it('should return the value for that variable for the Env.DEVELOPMENT config', () => {
-          process.env.DCL_DEFAULT_ENV = Env.DEVELOPMENT
+  describe('and getting a variable', () => {
+    describe('and all the envs are defined', () => {
+      describe('and the env is Env.DEVELOPMENT', () => {
+        describe('and the variable exists', () => {
+          it('should return the value for that variable for the Env.DEVELOPMENT config', () => {
+            process.env.DCL_DEFAULT_ENV = Env.DEVELOPMENT
+            config = createConfig(configByEnv)
+            expect(config.get('FOO')).toBe('bar-dev')
+          })
+        })
+      })
+
+      describe('and the env is Env.STAGING', () => {
+        describe('and the variable exists', () => {
+          it('should return the value for that variable for the Env.STAGING config', () => {
+            process.env.DCL_DEFAULT_ENV = Env.STAGING
+            config = createConfig(configByEnv)
+            expect(config.get('FOO')).toBe('bar-stg')
+          })
+        })
+      })
+
+      describe('and the env is Env.PRODUCTION', () => {
+        describe('and the variable exists', () => {
+          it('should return the value for that variable for the Env.PRODUCTION config', () => {
+            process.env.DCL_DEFAULT_ENV = Env.PRODUCTION
+            config = createConfig(configByEnv)
+            expect(config.get('FOO')).toBe('bar-prod')
+          })
+        })
+      })
+
+      describe('and the variable does not exist', () => {
+        it('should return undefined', () => {
           config = createConfig(configByEnv)
-          expect(config.get('FOO')).toBe('bar-dev')
+          expect(config.get('NON_EXISTENT')).toBe(undefined)
+        })
+
+        describe('and passing a default value', () => {
+          it('should return the default value', () => {
+            config = createConfig(configByEnv)
+            expect(config.get('NON_EXISTENT', 'someDefaultValue')).toBe(
+              'someDefaultValue'
+            )
+          })
         })
       })
     })
 
-    describe('and the env is Env.STAGING', () => {
-      describe('and getting value of a variable that exists', () => {
-        it('should return the value for that variable for the Env.STAGING config', () => {
-          process.env.DCL_DEFAULT_ENV = Env.STAGING
-          config = createConfig(configByEnv)
-          expect(config.get('FOO')).toBe('bar-stg')
-        })
-      })
-    })
-
-    describe('and the env is Env.PRODUCTION', () => {
-      describe('and getting value of a variable that exists', () => {
-        it('should return the value for that variable for the Env.PRODUCTION config', () => {
-          process.env.DCL_DEFAULT_ENV = Env.PRODUCTION
-          config = createConfig(configByEnv)
-          expect(config.get('FOO')).toBe('bar-prod')
-        })
-      })
-    })
-
-    describe('and getting value of a variable that does not exists', () => {
-      it('should return undefined', () => {
-        config = createConfig(configByEnv)
-        expect(config.get('NON_EXISTENT')).toBe(undefined)
-      })
-
-      describe('and and passing a default value', () => {
-        it('should return the default value', () => {
-          config = createConfig(configByEnv)
-          expect(config.get('NON_EXISTENT', 'someDefaultValue')).toBe(
-            'someDefaultValue'
-          )
-        })
-      })
-    })
-  })
-
-  describe('and an env is missing', () => {
-    const { stg: _, ...configWithMissingEnv } = configByEnv
-    describe('and getting the value of a variable', () => {
+    describe('and an env is missing', () => {
+      const { stg: _, ...configWithMissingEnv } = configByEnv
       it('should throw', () => {
         process.env.DCL_DEFAULT_ENV = Env.STAGING
         config = createConfig(configWithMissingEnv)
         expect(() => config.get('FOO')).toThrow()
       })
+    })
+  })
+
+  describe('and getting the env', () => {
+    it('should return the env currently used by the config', () => {
+      process.env.DCL_DEFAULT_ENV = Env.STAGING
+      config = createConfig(configByEnv)
+      expect(config.getEnv()).toBe(Env.STAGING)
+    })
+  })
+
+  describe('and checking if a given env is the current one', () => {
+    it('should return true if the given env is the current one', () => {
+      process.env.DCL_DEFAULT_ENV = Env.STAGING
+      config = createConfig(configByEnv)
+      expect(config.is(Env.STAGING)).toBe(true)
+    })
+    it('should return false if the given env is not the current one', () => {
+      process.env.DCL_DEFAULT_ENV = Env.STAGING
+      config = createConfig(configByEnv)
+      expect(config.is(Env.DEVELOPMENT)).toBe(false)
+      expect(config.is(Env.PRODUCTION)).toBe(false)
     })
   })
 })
